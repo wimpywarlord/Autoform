22646d5e1e98eea1f7ec5963e4c751795a0c0cb9
wimpywarlord <wimpywarlord@gmail.com>
2020-02-27 19:31:06 +0530

    added text presevation

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ app.use(express.static(__dirname + '/public'));
 app.get("/lawyer_form_intake",function(req,res){
 	res.render("lawyer_form_intake.ejs");
 });
+;
+app.get("/test",function(req,res){
+	res.render("test.ejs");
+});
 
+app.post("/add_questions_to_the_form",function(req,res){
+
+	// console.log(req.body);
+	res.render("adding_questions.ejs",{
+		"form_text" : req.body.yummy,
+	});
+});
 
 app.listen( process.env.PORT || 8000 , function(){
 	console.log("SERVER 8000 HAS STARTED");
