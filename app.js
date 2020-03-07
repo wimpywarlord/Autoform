107626b184b3432096c0f30f03d367ad22241a39
kvssankar <55633494+kvssankar@users.noreply.github.com>
2020-03-07 23:02:07 +0530

    designed home page

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ var global_variable_for_the_form_text;
 
 var app = express();
 app.use(bodyParser.urlencoded({extended : true}));
-
+app.use(express.static(__dirname + '/assests'));
 app.use(express.static(__dirname + '/public'));
 
+app.get("/",function(req,res){
+	res.render("home.ejs");
+})
 
 app.get("/lawyer_form_intake",function(req,res){
 	res.render("lawyer_form_intake.ejs");
